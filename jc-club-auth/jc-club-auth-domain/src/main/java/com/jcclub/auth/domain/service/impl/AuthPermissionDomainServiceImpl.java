b391539eceecfd8e24b0bc1da4005af0375a17cb
zhangchenyi <1494509856@qq.com>
2024-10-06 17:00:43 +0800

    一期项目开发完毕

--- jc-club-auth/jc-club-auth-domain/src/main/java/com/jcclub/auth/domain/service/impl/AuthPermissionDomainServiceImpl.java
+++ jc-club-auth/jc-club-auth-domain/src/main/java/com/jcclub/auth/domain/service/impl/AuthPermissionDomainServiceImpl.java
@@ -20,7 +20,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
